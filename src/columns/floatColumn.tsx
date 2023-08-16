4c8acc29cebcc03564b7fd22d62feb4649bb1423
chiucchi <gchiucchi01@gmail.com>
2023-08-16 16:52:34 -0300

    feat: add possibility to user input float with comma in floatcolumn

--- src/columns/floatColumn.tsx
+++ src/columns/floatColumn.tsx
@@ -3,9 +3,12 @@ import { createTextColumn } from './textColumn'
 export const floatColumn = createTextColumn<number | null>({
   alignRight: true,
   formatBlurredInput: (value) =>
-    typeof value === 'number' ? new Intl.NumberFormat().format(value) : '',
+    typeof value === 'number'
+      ? new Intl.NumberFormat('pt-BR').format(value)
+      : '',
   parseUserInput: (value) => {
-    const number = parseFloat(value)
+    const floatNumber = value.replace(',', '.')
+    const number = parseFloat(floatNumber)
     return !isNaN(number) ? number : null
   },
   parsePastedValue: (value) => {
